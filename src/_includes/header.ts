7045cf19b2e3c6c875426f1737998ba992027732
BovineBeta <bovinefbik@foxmail.com>
2025-07-22 13:55:47 +0800

    feat: let other page use blog.page.tsx

---
+++ src/_includes/header.ts
@@ -0,0 +1,9 @@
+export const header = {
+  bio: "Minimalism",
+  nav: {
+    site: "https://fbik.top",
+    github: "https://github.com/FBIKdot",
+    email: "mailto:fbik@fbik.top",
+    friends: "/friends",
+  },
+};
