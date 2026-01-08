c8652620326cd1ec1fbd9c941b555eb0c301a0a5
FBIK. <FBIKdot@users.noreply.github.com>
2026-01-08 12:49:33 +0800

    chore: change footer

--- src/_includes/footer.page.tsx
+++ src/_includes/footer.page.tsx
@@ -2,14 +2,7 @@ export default (obj: { children?: never }) => (
   <>
     <footer>
       <hr />
-      <a href="https://lume.land" target="_blank" rel="noopener noreferrer">
-        Lume
-      </a>
-      {" + "}
-      <a href="https://prose.sh" target="_blank" rel="noopener noreferrer">
-        prose.sh
-      </a>
-      {" = "}
+      Powered by{" "}
       <a
         href="https://github.com/FBIKdot/LumeProse"
         target="_blank"
